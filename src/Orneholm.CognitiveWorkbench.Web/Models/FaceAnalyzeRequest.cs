18e7df40a0ed794a0357fd98503931236b016003
Nicolas Robert <nicolas.robert@cellenza.com>
2020-03-10 16:39:18 +0100

    Face - Add DetectionModels parameter

--- src/Orneholm.CognitiveWorkbench.Web/Models/FaceAnalyzeRequest.cs
+++ src/Orneholm.CognitiveWorkbench.Web/Models/FaceAnalyzeRequest.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Orneholm.CognitiveWorkbench.Web.Models
 {
     public class FaceAnalyzeRequest
@@ -6,5 +8,14 @@ namespace Orneholm.CognitiveWorkbench.Web.Models
         public string FaceEndpoint { get; set; } = string.Empty;
 
         public string ImageUrl { get; set; } = string.Empty;
+        public FaceDetectionModel DetectionModel { get; set; } = FaceDetectionModel.detection_01;
+    }
+
+    public enum FaceDetectionModel
+    {
+        [Display(Name = "Detection 01 - Default")]
+        detection_01,
+        [Display(Name = "Detection 02 - Improved accuracy especially on small, side and blurry faces but no face attributes and landmarks")]
+        detection_02
     }
 }
